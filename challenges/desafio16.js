9f861a3d612c1e9703145fd9b4f997bbd13a48cb
Bianca Oura <biancaoura@gmail.com>
2023-02-07 22:25:26 -0300

    16 - add field in 1 document

---
+++ challenges/desafio16.js
@@ -0,0 +1,9 @@
+db.produtos.updateOne(
+  { nome: 'Big Mac' },
+  { $set: { ultimaModificacao: { $currentDate: { lastModified: true } } } }
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: 1 } },
+  { _id: 0, nome: 1 }
+);
